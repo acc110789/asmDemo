a2a92a21e4e9083b6624cdbb5dd541afc4715e21
luhaoyu <luhaoyu@kuaishou.com>
2020-09-16 00:07:09 +0800

    compiler变更

--- service-compiler/src/main/java/com/example/service_compiler/ServiceAnnotationProcessor.java
+++ service-compiler/src/main/java/com/example/service_compiler/ServiceAnnotationProcessor.java
@@ -1,17 +1,15 @@
 package com.example.service_compiler;
 
-import com.example.service_anno.Service;
 import com.example.service_anno.ServiceImpl;
 import com.google.auto.service.AutoService;
-
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -21,7 +19,10 @@ import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import javax.tools.FileObject;
@@ -72,31 +73,32 @@ public class ServiceAnnotationProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-        Set<? extends Element> serviceElements = roundEnvironment.getElementsAnnotatedWith(Service.class);
         Set<? extends Element> serviceImplElements = roundEnvironment.getElementsAnnotatedWith(ServiceImpl.class);
 
         JSONObject json = new JSONObject();
-
-        List<String> serviceNameList = new ArrayList<>();
-        for (Element element : serviceElements) {
-            if (element instanceof TypeElement) {
-                TypeElement typeElement = (TypeElement) element;
-                serviceNameList.add(typeElement.getQualifiedName().toString());
-            }
-        }
-        if (!serviceNameList.isEmpty()) {
-            json.put(INTERFACE, serviceNameList);
-        }
-
-        List<String> serviceImplNameList = new ArrayList<>();
         for (Element element : serviceImplElements) {
             TypeElement typeElement = (TypeElement) element;
-            serviceImplNameList.add(typeElement.getQualifiedName().toString());
-        }
-        if (!serviceImplNameList.isEmpty()) {
-            json.put(IMPL, serviceImplNameList);
-        }
+            List<? extends AnnotationMirror> annotationMirrors = typeElement.getAnnotationMirrors();
+            for (AnnotationMirror mirror : annotationMirrors) {
+                Map<? extends ExecutableElement, ? extends AnnotationValue> elementValues = mirror.getElementValues();
+                for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry: elementValues.entrySet()) {
+                    String key = entry.getKey().toString();
+                    if (!"service()".equals(key)) continue;
 
+                    String generalClassName = removeClassPostfix(entry.getValue().toString());
+                    Object value = entry.getValue().getValue();
+                    if (value instanceof Iterable) {
+                        Iterable iterable = (Iterable) value;
+                        for (Object one : iterable) {
+                            String className = removeClassPostfix(one.toString());
+                            json.put(className, typeElement.getQualifiedName().toString());
+                        }
+                    } else {
+                        json.put(generalClassName, typeElement.getQualifiedName().toString());
+                    }
+                }
+            }
+        }
         if (json.isEmpty()) return true;
 
         String result = json.toString();
@@ -114,11 +116,18 @@ public class ServiceAnnotationProcessor extends AbstractProcessor {
         return true;
     }
 
+    private String removeClassPostfix(String raw) {
+        if (raw == null) return null;
+        if (raw.endsWith(".class")) {
+            return raw.substring(0, raw.length() - 6);
+        }
+        return raw;
+    }
+
     @Override
     public Set<String> getSupportedAnnotationTypes() {
         Set<String> types = new HashSet<>();
         types.add(ServiceImpl.class.getCanonicalName());
-        types.add(Service.class.getCanonicalName());
         return types;
     }
 
